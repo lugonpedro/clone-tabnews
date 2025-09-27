122eec958e6c4c538baf977346730a399a94de58
Pedro Lugon <pedromotta20@gmail.com>
2025-09-27 13:05:48 -0300

    feat: add all routes tests for `status` and `migrations`

---
+++ tests/integration/api/v1/migrations/delete.test.js
@@ -0,0 +1,25 @@
+import orchestrator from "tests/orchestrator.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+});
+
+describe("DELETE /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving pending migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: "DELETE",
+      });
+      expect(response.status).toBe(405);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        name: "MethodNotAllowedError",
+        message: "Method not allowed for this endpoint",
+        action: "Check that the HTTP method sent is valid",
+        status_code: 405,
+      });
+    });
+  });
+});
