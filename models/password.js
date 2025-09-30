9464a83bb1d750c5df73744c0c8b7319608965d9
Pedro Lugon <pedromotta20@gmail.com>
2025-09-29 22:35:36 -0300

    feat: add password pepper in `password.js`

--- models/password.js
+++ models/password.js
@@ -2,11 +2,20 @@ import bcryptjs from "bcryptjs";
 
 async function hash(password) {
   const rounds = getNumberOfRounds();
-  return await bcryptjs.hash(password, rounds);
+  const passwordPepper = addPepperPassword(password);
+  return await bcryptjs.hash(passwordPepper, rounds);
 }
 
 function getNumberOfRounds() {
   return process.env.NODE_ENV === "production" ? 14 : 1;
+}
+
+function addPepperPassword(password) {
+  const pepper = process.env.HASH_PEPPER || "";
+
+  const result = password.concat(pepper);
+
+  return result;
 }
 
 async function compare(providedPassword, storedPassword) {
