be486dcb3aa82ee1698c0ac1b7d57fb762a139c2
Pedro Lugon <pedromotta20@gmail.com>
2026-05-03 03:46:51 -0300

    feat: allow `update:user:others` to update other users

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -102,6 +102,11 @@ async function createActivationToken(userId) {
   return await activation.create(userId);
 }
 
+async function addFeaturesToUser(userObject, features) {
+  const updatedUser = await user.addFeatures(userObject.id, features);
+  return updatedUser;
+}
+
 const orchestrator = {
   waitForAllServices,
   clearDatabase,
@@ -113,6 +118,7 @@ const orchestrator = {
   extractUUID,
   activateUser,
   createActivationToken,
+  addFeaturesToUser,
 };
 
 export default orchestrator;
