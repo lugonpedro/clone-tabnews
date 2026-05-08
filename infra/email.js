edafbb57a9d485527efc8fbd2a2a2561b5f03c6a
Pedro Lugon <pedromotta20@gmail.com>
2026-05-08 14:55:02 -0300

    feat: add resend as mailer

--- infra/email.js
+++ infra/email.js
@@ -1,5 +1,7 @@
 import nodemailer from "nodemailer";
 import { ServiceError } from "./errors";
+import Resend from "resend";
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_SMTP_HOST,
@@ -13,7 +15,14 @@ const transporter = nodemailer.createTransport({
 
 async function send(emailOptions) {
   try {
-    await transporter.sendMail(emailOptions);
+    // await transporter.sendMail(emailOptions);
+
+    resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: emailOptions.to,
+      subject: emailOptions.suject,
+      text: emailOptions.text,
+    });
   } catch (error) {
     throw new ServiceError({
       message: "Error when trying to send email",
